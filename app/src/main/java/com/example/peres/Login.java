26704d40a25061759ab55255e97a5908db110eb7
mateussperess <mateuspereslopesl@outlook.com>
2023-01-04 09:29:01 -0300

    Removido a barra de título da activity quando roda a aplicação

--- app/src/main/java/com/example/peres/Login.java
+++ app/src/main/java/com/example/peres/Login.java
@@ -13,6 +13,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
     }
 
     public void launchMainActivity(View view) {
